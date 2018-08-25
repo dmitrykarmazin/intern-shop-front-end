4327ad86d385d6be839b87c12cca412f6325aa8a
Pisker Mykola <macbookpro@Piskers-MacBook-Pro.local>
2018-08-25 19:56:46 +0300

    first commit

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MaterialModule} from '../material/material.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule
   ],
   declarations: []
 })
