d6cd13411552b8e6eecb5bc1817ff40334b0f8fa
Elvin Agaragimow <e.agaragimow@gmail.com>
2018-08-26 19:08:32 +0300

    [NgRx] Products rendered

---
+++ src/app/shop/containers/products-view/products-view.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Product } from '../../../shared/models/product.model';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-products-view',
+  templateUrl: './products-view.component.html',
+  styleUrls: ['./products-view.component.css']
+})
+export class ProductsViewComponent implements OnInit {
+
+  @Input() products$: Observable<Product[]>;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
