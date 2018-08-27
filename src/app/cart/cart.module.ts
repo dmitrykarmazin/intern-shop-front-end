bbfed5e9ad130d2fe0197e5db3745a37188ee602
tanya210392 <tanya210392@gmail.com>
2018-08-27 09:47:07 +0300

    IS-cart-page #quantity -> ngModel quantity

--- src/app/cart/cart.module.ts
+++ src/app/cart/cart.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { MaterialModule } from '../material/material.module';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -7,10 +8,12 @@ import { CartPageComponent } from './containers/cart-page/cart-page.component';
 import { CartItemsComponent } from './components/cart-items/cart-items.component';
 import { CartTotalComponent } from './components/cart-total/cart-total.component';
 
+
 @NgModule({
   imports: [
     CommonModule,
     MaterialModule,
+    FormsModule,
     HttpClientModule
   ],
   declarations: [
