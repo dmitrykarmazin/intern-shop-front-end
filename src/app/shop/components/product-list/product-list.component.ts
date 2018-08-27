9977340140e687bbb08fe6cddcbc0289ad8feb59
Kuzmin Maksym <kuzmin.maksym@gmail.com>
2018-08-27 10:18:32 +0300

    conflicts merged

--- src/app/shop/components/product-list/product-list.component.ts
+++ src/app/shop/components/product-list/product-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Observable, of, from } from 'rxjs';
 
 import Product from '../../models/product';
@@ -13,63 +13,18 @@ export class ProductListComponent {
   @Input() viewMode$: Observable<string>;
   @Input() products$: Observable<Product[]>;
 
-  private isListType: boolean = false;
+  @Output() emitAddToCard: EventEmitter<string> = new EventEmitter<string>();
+
+  private isListType: boolean = true;
 
   constructor() {
-     /* TODO include before work  */
-     // this.viewMode$.subscribe((listType: string) => this.isListType = (listType === 'list'));
 
-     // TODO - only for testing
-     this.viewMode$ = from('list');
-     this.products$ = of([
-       {
-         id: '111',
-         title: 'my test product',
-         description: `my very first testing product my very first testing product my very first testing product
-            my very first testing product my very first testing product my very first testing product`,
-         category_id: '3',
-         category_title: 'test',
-         price: 99.99,
-         stock: 15
-       },
-       {
-        id: '222',
-         title: 'my test product',
-         description: `my very first testing product my very first testing product my very
-            first testing product my very first testing product my very first testing product`,
-         category_id: '3',
-         category_title: 'test',
-         price: 99.99,
-         stock: 10
-       },
-       {
-        id: '333',
-         title: 'my test product',
-         description: 'my very first testing product',
-         category_id: '3',
-         category_title: 'test',
-         price: 99.99,
-         stock: 0
-       },
-       {
-        id: '444',
-        title: 'my test product',
-        description: 'my very first testing product',
-        category_id: '3',
-        category_title: 'test',
-        price: 99.99,
-        stock: 18
-      },
-      {
-        id: '555',
-        title: 'my test product',
-        description: 'my very first testing product',
-        category_id: '3',
-        category_title: 'test',
-        price: 99.99,
-        stock: 33
-      }
-     ]);
-     // End temp data
+    // this.viewMode$.subscribe((data: string) => this.isListType = data === 'list' ? true : false);
+
+  }
+
+  private addToCard($event: string): void {
+    console.log('Buy me!!!. My id:', $event);
+    this.emitAddToCard.emit($event);
   }
 }
