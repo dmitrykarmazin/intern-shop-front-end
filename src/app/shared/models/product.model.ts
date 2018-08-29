1a0860cc1f2a72396cbe24688e50cdee45df9215
dmitrykarmazin <dmitriy.karmazin92@gmail.com>
2018-08-29 12:55:51 +0300

    bug fix, update product model

--- src/app/shared/models/product.model.ts
+++ src/app/shared/models/product.model.ts
@@ -4,6 +4,7 @@ export class Product {
     public title: string,
     public description: string,
     public category_id: string,
+    public thumbnail: string,
     public category_title: string,
     public price: string,
     public stock: number
